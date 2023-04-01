64751b167a5677b5ffeea269b6ce4bd0ddb8c518
LAPTOP-GC98O71B\elena <elenadiana793@gmail.com>
2023-04-01 19:05:44 +0300

    Am adaugat clasa ServiciiMedicale

---
+++ src/main/java/ro/pao/model/ServiciiMedicale.java
@@ -0,0 +1,80 @@
+package ro.pao.model;
+
+import java.util.Date;
+
+public class ServiciiMedicale {
+    private String numeServiciu;
+    private String descriere;
+    private double cost;
+    private int durata;
+    private String personalMedical;
+    private String echipament;
+    private Date disponibilitate;
+
+    public ServiciiMedicale(String numeServiciu, String descriere, double cost, int durata,
+                            String personalMedical, String echipament, Date disponibilitate){
+        this.numeServiciu = numeServiciu;
+        this.descriere = descriere;
+        this.cost = cost;
+        this.durata = durata;
+        this.personalMedical = personalMedical;
+        this.echipament = echipament;
+        this.disponibilitate = disponibilitate;
+    }
+
+    public String getNumeServiciu() {
+        return numeServiciu;
+    }
+
+    public void setNumeServiciu(String numeServiciu) {
+        this.numeServiciu = numeServiciu;
+    }
+
+    public String getDescriere() {
+        return descriere;
+    }
+
+    public void setDescriere(String descriere) {
+        this.descriere = descriere;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getDurata() {
+        return durata;
+    }
+
+    public void setDurata(int durata) {
+        this.durata = durata;
+    }
+
+    public String getPersonalMedical() {
+        return personalMedical;
+    }
+
+    public void setPersonalMedical(String personalMedical) {
+        this.personalMedical = personalMedical;
+    }
+
+    public String getEchipament() {
+        return echipament;
+    }
+
+    public void setEchipament(String echipament) {
+        this.echipament = echipament;
+    }
+
+    public Date getDisponibilitate() {
+        return disponibilitate;
+    }
+
+    public void setDisponibilitate(Date disponibilitate) {
+        this.disponibilitate = disponibilitate;
+    }
+}
