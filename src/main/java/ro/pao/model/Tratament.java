e42a55c31f8a20f95e0daf31bb77f0c05f868884
LAPTOP-GC98O71B\elena <elenadiana793@gmail.com>
2023-04-01 19:33:51 +0300

    Am adaugat clasa Tratament

---
+++ src/main/java/ro/pao/model/Tratament.java
@@ -0,0 +1,60 @@
+package ro.pao.model;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+public class Tratament {
+    private Medici medic;
+    private Pacient pacient;
+    private LocalDate dataPrescrierii;
+    private Medicament medicamente;
+    private Duration durata;
+
+    public Tratament(Medici medic, Pacient pacient, LocalDate dataPrescrierii, Medicament medicamente, Duration durata){
+        this.medic = medic;
+        this.pacient = pacient;
+        this.dataPrescrierii = dataPrescrierii;
+        this.medicamente = medicamente;
+        this.durata = durata;
+    }
+
+    public Medici getMedic() {
+        return medic;
+    }
+
+    public void setMedic(Medici medic) {
+        this.medic = medic;
+    }
+
+    public Pacient getPacient() {
+        return pacient;
+    }
+
+    public void setPacient(Pacient pacient) {
+        this.pacient = pacient;
+    }
+
+    public LocalDate getDataPrescrierii() {
+        return dataPrescrierii;
+    }
+
+    public void setDataPrescrierii(LocalDate dataPrescrierii) {
+        this.dataPrescrierii = dataPrescrierii;
+    }
+
+    public Medicament getMedicamente() {
+        return medicamente;
+    }
+
+    public void setMedicamente(Medicament medicamente) {
+        this.medicamente = medicamente;
+    }
+
+    public Duration getDurata() {
+        return durata;
+    }
+
+    public void setDurata(Duration durata) {
+        this.durata = durata;
+    }
+}
