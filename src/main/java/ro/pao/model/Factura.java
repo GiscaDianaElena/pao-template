bce0b89e2e9fe7e76fb62b6d45f94022bf4073c8
LAPTOP-GC98O71B\elena <elenadiana793@gmail.com>
2023-04-01 19:30:20 +0300

    Am adaugat clasa Factura

---
+++ src/main/java/ro/pao/model/Factura.java
@@ -0,0 +1,80 @@
+package ro.pao.model;
+
+import java.time.LocalDateTime;
+
+public class Factura {
+    private int numarFactura;
+    private LocalDateTime dataEmiterii;
+    private Pacient pacient;
+    private Medici medic;
+    private ServiciiMedicale serviciiMedicale;
+    private double total;
+    private Status status;
+
+    public Factura(int numarFactura, LocalDateTime dataEmiterii, Pacient pacient, Medici medic,
+                   ServiciiMedicale serviciiMedicale, double total, Status status){
+        this.numarFactura = numarFactura;
+        this.dataEmiterii = dataEmiterii;
+        this.pacient = pacient;
+        this.medic = medic;
+        this.serviciiMedicale = serviciiMedicale;
+        this.total = total;
+        this.status = status;
+    }
+
+    public int getNumarFactura() {
+        return numarFactura;
+    }
+
+    public void setNumarFactura(int numarFactura) {
+        this.numarFactura = numarFactura;
+    }
+
+    public LocalDateTime getDataEmiterii() {
+        return dataEmiterii;
+    }
+
+    public void setDataEmiterii(LocalDateTime dataEmiterii) {
+        this.dataEmiterii = dataEmiterii;
+    }
+
+    public Pacient getPacient() {
+        return pacient;
+    }
+
+    public void setPacient(Pacient pacient) {
+        this.pacient = pacient;
+    }
+
+    public Medici getMedic() {
+        return medic;
+    }
+
+    public void setMedic(Medici medic) {
+        this.medic = medic;
+    }
+
+    public ServiciiMedicale getServiciiMedicale() {
+        return serviciiMedicale;
+    }
+
+    public void setServiciiMedicale(ServiciiMedicale serviciiMedicale) {
+        this.serviciiMedicale = serviciiMedicale;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
