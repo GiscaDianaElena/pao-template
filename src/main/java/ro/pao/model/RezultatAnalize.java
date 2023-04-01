ebcfad774b4070c4c2f1763ee37bbddc906a5bb7
LAPTOP-GC98O71B\elena <elenadiana793@gmail.com>
2023-04-01 19:35:46 +0300

    Am adaugat clasa RezultatAnalize

---
+++ src/main/java/ro/pao/model/RezultatAnalize.java
@@ -0,0 +1,59 @@
+package ro.pao.model;
+
+import java.time.LocalDate;
+
+public class RezultatAnalize {
+    private Pacient pacient;
+    private Medici medic;
+    private LocalDate data;
+    private String tipAnalize;
+    private String rezultate;
+
+    public RezultatAnalize(Pacient pacient, Medici medic, LocalDate data, String tipAnalize, String rezultate){
+        this.pacient = pacient;
+        this.medic = medic;
+        this.data = data;
+        this.tipAnalize = tipAnalize;
+        this.rezultate = rezultate;
+    }
+
+    public Pacient getPacient() {
+        return pacient;
+    }
+
+    public void setPacient(Pacient pacient) {
+        this.pacient = pacient;
+    }
+
+    public Medici getMedic() {
+        return medic;
+    }
+
+    public void setMedic(Medici medic) {
+        this.medic = medic;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getTipAnalize() {
+        return tipAnalize;
+    }
+
+    public void setTipAnalize(String tipAnalize) {
+        this.tipAnalize = tipAnalize;
+    }
+
+    public String getRezultate() {
+        return rezultate;
+    }
+
+    public void setRezultate(String rezultate) {
+        this.rezultate = rezultate;
+    }
+}
