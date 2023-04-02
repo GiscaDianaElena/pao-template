5f2b51e47f20ae45e499fb012ff6d1ea4c8dd55b
LAPTOP-GC98O71B\elena <elenadiana793@gmail.com>
2023-04-02 10:50:04 +0300

    Am creat clasa ServiciiMedicaleServiceService si in ServiciiMedicaleServiceImpl am implementat metodele din clasa Service.

---
+++ src/main/java/ro/pao/service/ServiciiMedicaleService.java
@@ -0,0 +1,23 @@
+package ro.pao.service;
+
+import ro.pao.model.ServiciiMedicale;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ServiciiMedicaleService {
+    static List<ServiciiMedicale> serviciiMedicaleList = new ArrayList<>();
+    // o lista unde stocam toate serviciile medicale
+
+    List<ServiciiMedicale> showAllServiciiMedicale();
+
+    Optional<ServiciiMedicale> getServiciiMedicalebyId(UUID id);
+
+    List<ServiciiMedicale> getAllFromList();
+
+    void addAllFromGivenList(List<ServiciiMedicale> serviciiMedicaleList);
+
+    void addOnlyOne(ServiciiMedicale serviciiMedicale);
+}
