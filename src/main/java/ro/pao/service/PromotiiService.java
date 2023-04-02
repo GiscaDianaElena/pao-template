20bc3704b5d7911aa0ba5cdb289704b639bb5b9e
LAPTOP-GC98O71B\elena <elenadiana793@gmail.com>
2023-04-02 10:43:58 +0300

    Am creat clasa PromotiiService si in PromotiiServiceImpl am implementat metodele din clasa Service.

---
+++ src/main/java/ro/pao/service/PromotiiService.java
@@ -0,0 +1,23 @@
+package ro.pao.service;
+
+import ro.pao.model.Promotii;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PromotiiService {
+    static List<Promotii> promotiiList = new ArrayList<>();
+    // o lista unde stocam toate promotiile
+
+    List<Promotii> showAllPromotii();
+
+    Optional<Promotii> getPromotiibyId(UUID id);
+
+    List<Promotii> getAllFromList();
+
+    void addAllFromGivenList(List<Promotii> promotiiList);
+
+    void addOnlyOne(Promotii promotii);
+}
---
+++ src/main/java/ro/pao/service/impl/PromotiiServiceImpl.java
@@ -0,0 +1,38 @@
+package ro.pao.service.impl;
+
+import ro.pao.model.Programare;
+import ro.pao.model.Promotii;
+import ro.pao.service.PromotiiService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class PromotiiServiceImpl implements PromotiiService {
+    private static List<Promotii> promotiiList = new ArrayList<>();
+
+    public List<Promotii> showAllPromotii() {
+        return promotiiList;
+    }
+
+    @Override
+    public Optional<Promotii> getPromotiibyId(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Promotii> getAllFromList() {
+        return promotiiList;
+    }
+
+    @Override
+    public void addAllFromGivenList(List<Promotii> promotiiList) {
+        promotiiList.addAll(promotiiList);
+    }
+
+    @Override
+    public void addOnlyOne(Promotii promotii) {
+        promotiiList.add(null);
+    }
+}
